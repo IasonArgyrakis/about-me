20cfa67dc4b346ed8be6b97e0577f67582caf3a2
JasonArgyrakis <jasonargirakis@gmail.com>
2024-06-21 14:27:52 +0300

    create vuetify command

---
+++ vue-cv/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script setup>
+  //
+import AppFooter from "@/components/AppFooter.vue";
+  import HelloWorld from "@/components/HelloWorld.vue";
+</script>
